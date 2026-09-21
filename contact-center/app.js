cfe67a6083d199341b67349013eac0cfb3236dcb
MrProfessorr <mrprofessorr88@gmail.com>
2026-09-21 12:51:45 +0800

    Update app.js

--- contact-center/app.js
+++ contact-center/app.js
@@ -1,9 +1,7 @@
 import {
-  initializeApp
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-
+  db
+} from "./firebase.js";
 import {
-  getDatabase,
   ref,
   onValue,
   set,
@@ -13,38 +11,6 @@ import {
   serverTimestamp,
   increment
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
-
-const firebaseConfig = {
-
-  apiKey:
-    "AIzaSyDl9m6icGPQ_vPh030_-vwBrYFL9sV1jZ8",
-
-  authDomain:
-    "support-pwa.firebaseapp.com",
-
-  databaseURL:
-    "https://support-pwa-default-rtdb.asia-southeast1.firebasedatabase.app",
-
-  projectId:
-    "support-pwa",
-
-  storageBucket:
-    "support-pwa.firebasestorage.app",
-
-  messagingSenderId:
-    "1036053746669",
-
-  appId:
-    "1:1036053746669:web:952df405b937cefe6d8de4"
-};
-
-
-const app =
-  initializeApp(firebaseConfig);
-
-
-const db =
-  getDatabase(app);
 
 let loaderMinMs =
   1500;

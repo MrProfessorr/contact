5c42164c3ae695aa07a03f68380ce8bb5f58cbe4
MrProfessorr <mrprofessorr88@gmail.com>
2026-09-26 13:23:59 +0800

    Update shared-ui.js

--- contact-center/admin/shared-ui.js
+++ contact-center/admin/shared-ui.js
@@ -3740,43 +3740,86 @@ adminPasswordModal.innerHTML = `
       </div>
 
 
-      <div
-        class="admin-password-field"
+<div
+  class="admin-password-field"
+>
+  <label
+    id="adminNewPasswordLabel"
+    for="adminNewPassword"
+  >
+    New Password
+  </label>
+
+  <div
+    class="admin-password-input-wrap"
+  >
+
+    <span
+      class="admin-password-input-icon"
+      aria-hidden="true"
+    >
+      <svg
+        viewBox="0 0 1024 1024"
+        width="14"
+        height="14"
       >
-        <label
-          id="adminNewPasswordLabel"
-          for="adminNewPassword"
-        >
-          New Password
-        </label>
+        <path
+          d="M832 464h-68V240c0-70.7-57.3-128-128-128H388c-70.7 0-128 57.3-128 128v224h-68c-17.7 0-32 14.3-32 32v384c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 32-32V496c0-17.7-14.3-32-32-32zM332 240c0-30.9 25.1-56 56-56h248c30.9 0 56 25.1 56 56v224H332V240zm460 600H232V536h560v304zM484 701v53c0 4.4 3.6 8 8 8h40c4.4 0 8-3.6 8-8v-53a48.01 48.01 0 10-56 0z"
+          fill="currentColor"
+        />
+      </svg>
+    </span>
 
-        <input
-          id="adminNewPassword"
-          class="shared-input"
-          type="password"
-          autocomplete="new-password"
-          required
-        >
-      </div>
+    <input
+      id="adminNewPassword"
+      class="shared-input admin-password-icon-input"
+      type="password"
+      autocomplete="new-password"
+      required
+    >
 
+  </div>
+</div>
 
-      <div
-        class="admin-password-field"
+<div
+  class="admin-password-field"
+>
+  <label
+    for="adminConfirmPassword"
+  >
+    Confirm New Password
+  </label>
+
+  <div
+    class="admin-password-input-wrap"
+  >
+
+    <span
+      class="admin-password-input-icon"
+      aria-hidden="true"
+    >
+      <svg
+        viewBox="0 0 1024 1024"
+        width="14"
+        height="14"
       >
-        <label
-          for="adminConfirmPassword"
-        >
-          Confirm New Password
-        </label>
+        <path
+          d="M832 464h-68V240c0-70.7-57.3-128-128-128H388c-70.7 0-128 57.3-128 128v224h-68c-17.7 0-32 14.3-32 32v384c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 32-32V496c0-17.7-14.3-32-32-32zM332 240c0-30.9 25.1-56 56-56h248c30.9 0 56 25.1 56 56v224H332V240zm460 600H232V536h560v304zM484 701v53c0 4.4 3.6 8 8 8h40c4.4 0 8-3.6 8-8v-53a48.01 48.01 0 10-56 0z"
+          fill="currentColor"
+        />
+      </svg>
+    </span>
 
-        <input
-          id="adminConfirmPassword"
-          class="shared-input"
-          type="password"
-          autocomplete="new-password"
-          required
-        >
-      </div>
+    <input
+      id="adminConfirmPassword"
+      class="shared-input admin-password-icon-input"
+      type="password"
+      autocomplete="new-password"
+      required
+    >
+
+  </div>
+</div>
 
 
       <div
